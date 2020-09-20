94f350ac2a61debda2d63c50fc9f7529e2e21ac7
Attednev <damian.gasser@gmail.com>
2020-09-20 14:13:04 +0200

    Fixed server services

--- src/Utility/ServerUtility.java
+++ src/Utility/ServerUtility.java
@@ -1,7 +1,5 @@
 package Utility;
 
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,53 +8,61 @@ import java.util.TimeZone;
 
 public class ServerUtility {
 
-    // Function that executes a given command with its arguments
     public static int executeCommand(Socket socket, String[] args) {
         String command = args[0].toLowerCase();
         switch (command) {
+            case "exit":
+                return -1;
             case "help":
-                return ServerUtility.sendHelp(socket);
+                return ServerUtility.sendHelpMessage(socket);
             case "tz":
-                return ServerUtility.sendTimeZone(socket, args[1]);
+                if (args.length >= 2) {
+                    String timeZone = args[1];
+                    return ServerUtility.sendTimeZone(socket, timeZone);
+                }
+                break;
             case "tn":
                 if (args.length >= 4) {
                     int from = Integer.parseInt(args[2]);
                     int to = Integer.parseInt(args[3]);
-                    return ServerUtility.sendTranslatedNumber(socket, args[1], from, to);
+                    String number = args[1];
+                    return ServerUtility.sendTranslatedNumber(socket, number, from, to);
                 }
                 break;
-            case "exit":
-                return -1;
         }
-        SocketUtility.sendMessage(socket, "Unknown command! Type 'help' to get help");
+        SocketUtility.sendMessage(socket, "To few arguments! Type 'help' for more information");
         return 0;
     }
 
-    // Function that sends a predefined text to the client that shows him the commands
-    private static int sendHelp(Socket socket) {
+    private static int sendHelpMessage(Socket socket) {
         return SocketUtility.sendMessage(socket, "help - Zeige diese Anzeige an \n" +
                 "tz <zone> - Zeit in der Zeitzone \n" +
                 "tn <Zahl> <StartBasis> <EndBasis> - Umwandlung der Zahl <Zahl> aus dem System <StartBasis> ins System <EndBasis> \n" +
                 "exit - Unterbrich die Verbindung");
     }
 
-    // TODO:
-    // Function that sends the time and date of a specific time zone to the client
     private static int sendTimeZone(Socket socket, String timeZone) {
-        String zone = timeZone.toLowerCase();
-        Date date = new Date();
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        df.setTimeZone(TimeZone.getTimeZone(zone));
-        return SocketUtility.sendMessage(socket, df.format(date));
-    }
+        int indexCountry = timeZone.indexOf('/') + 1;
+        char continentFirstChar = Character.toUpperCase(timeZone.charAt(0));
+        char countryFirstChar = Character.toUpperCase(timeZone.charAt(0));
+        String continentSubString = timeZone.substring(1, indexCountry).toLowerCase();
+        String countrySubString = timeZone.substring(indexCountry + 1).toLowerCase();
+        String timeZoneCamelCase = continentFirstChar + continentSubString + countryFirstChar + countrySubString;
 
+        DateFormat df = new SimpleDateFormat("HH:mm:ss");
+        df.setTimeZone(TimeZone.getTimeZone(timeZoneCamelCase));
+
+        return SocketUtility.sendMessage(socket, df.format(new Date()));
+    }
 
     private static int sendTranslatedNumber(Socket socket, String num, int from, int to) {
-        int dec = Integer.parseInt(num, from);
-        String number = Integer.toString(dec, to);
-        return SocketUtility.sendMessage(socket, number);
+        try {
+            int dec = Integer.parseInt(num, from);
+            String number = Integer.toString(dec, to);
+            return SocketUtility.sendMessage(socket, number);
+        } catch (NumberFormatException e) {
+            return SocketUtility.sendMessage(socket, "Invalid number system!");
+        }
     }
-
-
 
 }
